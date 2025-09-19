0823b4197195020a20cb107077b8961ab2b00429
Alexandr <Alexandr.p@fx.gl>
2025-09-19 15:46:49 +0300

    Polygon-colliders-instead-buttons-for-region-info

---
+++ Assets/Code/Province.cs
@@ -0,0 +1,65 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class Province : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [Header("Подсветка")]
+    public Color baseColor = Color.white;
+    public Color highlightColor = Color.yellow;
+
+    [Header("ID для панели")]
+    public string provinceId;
+
+    private Material instanceMaterial;
+    private MeshRenderer meshRenderer;
+    private MeshCollider meshCollider;
+
+    public PanelAnimator panelAnimator;
+    public PanelTextLoader textLoader;
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshCollider = GetComponent<MeshCollider>();
+
+        // Делаем уникальный материал для провинции
+        instanceMaterial = new Material(meshRenderer.sharedMaterial);
+        meshRenderer.material = instanceMaterial;
+        instanceMaterial.color = baseColor;
+
+        // Подключаем Mesh к коллайдеру (только для Raycast)
+        meshCollider.sharedMesh = GetComponent<MeshFilter>().sharedMesh;
+        meshCollider.convex = false;   // пусть остаётся в "естественном" виде
+        // isTrigger не ставим! иначе Unity ругнётся
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        instanceMaterial.color = highlightColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        instanceMaterial.color = baseColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        bool isMobile = ScreenChecker.IsMobileScreen();
+
+        textLoader.LoadText(provinceId);
+        panelAnimator.ShowPanel(isMobile);
+    }
+
+/*    private void OnButtonClick()
+    {
+        bool isMobile = ScreenChecker.IsMobileScreen();
+
+        textLoader.LoadText(buttonId);
+        panelAnimator.ShowPanel(isMobile);
+    }*/
+}
